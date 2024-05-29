afb712659aa32b476327596fa73188e0952cd904
Cristian Lopez <losecris@gmail.com>
2024-05-28 23:49:53 -0300

    Estilizando a  sidebar

--- src/styles.ts
+++ src/styles.ts
@@ -10,9 +10,11 @@ const EstiloGlobal = createGlobalStyle`
   margin: 0;
   padding:0;
   font-family: "Inter", sans-serif;
+  list-style: none;
 }
 body {
   padding-top: 80px;
+  padding-bottom: 80px;
 
   @media (max-width: 768px) {
     padding-top: 16px;
@@ -37,9 +39,5 @@ export const Container = styled.div`
   @media (max-width: 768px) {
     max-width: 80%;
     display: block;
-  }
-
-  img {
-    max-width: 100%;
   }
 `;
